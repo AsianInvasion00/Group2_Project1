05863cfbc32b5dc1df1f6c27072dc5b52d4840b8
AsianInvasion00 <unconfigured@null.spigotmc.org>
2023-05-01 21:34:05 -0400

    config not working, it is pushing api key

--- Config.py
+++ Config.py
@@ -2,4 +2,4 @@
 fiscaldata_key = "YOUR KEY HERE"
 nytimes_key = "YOUR KEY HERE"
 kaggle_key = "YOUR KEY HERE"
-fredaccount_key = "6c3d32585082258d0b9ca2a01baa9a79"
+fredaccount_key = "YOUR KEY HERE"
